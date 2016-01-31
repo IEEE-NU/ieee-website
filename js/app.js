5cabfca0ea73c78f4a7810873f16c6a32dc68371
Chris Chen <cschen13@gmail.com>
2016-01-31 12:58:07 -0600

    Changed font, filtered picture, added new headshots

--- js/app.js
+++ js/app.js
@@ -19,34 +19,34 @@ ieeeApp.controller('HomeCtrl', function($scope) {
 		{
 			name: 'Kushal Gourikrishna',
 			position: 'Co-President',
-			img: 'assets/headshots/Yeezy.jpg'
+			img: 'assets/headshots/Kush.jpg'
 		},
 
 		{
 			name: 'Adam He',
 			position: 'Co-President',
-			img: 'assets/headshots/Yeezy.jpg'
+			img: 'assets/headshots/Adam.jpg'
 		},
 
 		{
 			name: 'Curtis Wang',
 			position: 'Wise Old Man',
-			img: 'assets/headshots/Yeezy.jpg'
+			img: 'assets/headshots/Curtis.jpg'
 		},
 
 		{
 			name: 'Kevin Chen',
-			img: 'assets/headshots/Yeezy.jpg'
+			img: 'assets/headshots/Kevin_Chen.jpg'
 		},
 
 		{
 			name: 'Kapil Garg',
-			img: 'assets/headshots/Yeezy.jpg'
+			img: 'assets/headshots/Kapil.jpg'
 		},
 
 		{
 			name: 'Bryanna Yeh',
-			img: 'assets/headshots/Yeezy.jpg'
+			img: 'assets/headshots/Bryanna.jpg'
 		},
 
 		{
@@ -56,17 +56,17 @@ ieeeApp.controller('HomeCtrl', function($scope) {
 
 		{
 			name: 'Kevin Wilde',
-			img: 'assets/headshots/Kevin.jpg'
+			img: 'assets/headshots/Kevin_Wilde.jpg'
 		},
 
 		{
 			name: 'Holliday Schuler',
-			img: 'assets/headshots/Yeezy.jpg'
+			img: 'assets/headshots/Holliday.jpg'
 		},
 
 		{
 			name: 'Ricardo Rivas',
-			img: 'assets/headshots/Yeezy.jpg'
+			img: 'assets/headshots/Ricardo.jpg'
 		},
 
 		{
